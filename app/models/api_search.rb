1080f85edc34854cc1434a5c952e705e6d1228be
Patrick MacMurchy <pmm4654@gmail.com>
2018-10-03 08:44:11 -0600

    Add scopes to ApiSearch

--- app/models/api_search.rb
+++ app/models/api_search.rb
@@ -1,2 +1,12 @@
 class ApiSearch < ApplicationRecord
+  scope :last_5_unique, -> { limit(5).order('id desc').group('user_input').count }
+
+  scope :with_versions, -> do 
+    select('api_searches.id, api_searches.user_input, COUNT(versions.id) as version_count')
+      .joins(:versions)
+      .group('api_searches.id')
+  end
+
+  scope :top_5_searches, -> { with_versions.limit(5).order('version_count desc') }
+  has_paper_trail
 end
