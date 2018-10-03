e970f57cfc0e253382f54cd5720d88e2e284505d
Patrick MacMurchy <pmm4654@gmail.com>
2018-10-03 08:44:54 -0600

    Remove unused scope

--- app/models/api_search.rb
+++ app/models/api_search.rb
@@ -1,6 +1,4 @@
 class ApiSearch < ApplicationRecord
-  scope :last_5_unique, -> { limit(5).order('id desc').group('user_input').count }
-
   scope :with_versions, -> do 
     select('api_searches.id, api_searches.user_input, COUNT(versions.id) as version_count')
       .joins(:versions)
